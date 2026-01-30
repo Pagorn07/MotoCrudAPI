b22e1c954ee9941901032b711eec93ebb7a06eb8
Pagorn07 <pabloarbos1993@gmail.com>
2026-01-30 22:24:24 +0100

    Add collection test and use Foundry for random data

---
+++ src/Factory/MotorcycleFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Motorcycle;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Motorcycle>
+ */
+final class MotorcycleFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Motorcycle::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'brand' => self::faker()->text(40),
+            'engineCapacity' => self::faker()->numberBetween(125, 1200),
+            'extras' => [],
+            'limitedEdition' => self::faker()->boolean(),
+            'model' => self::faker()->text(50),
+            'type' => self::faker()->text(50),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Motorcycle $motorcycle): void {})
+        ;
+    }
+}
--- tests/Api/MotorcycleTest.php
+++ tests/Api/MotorcycleTest.php
@@ -4,9 +4,45 @@ namespace App\Tests\Api;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\Motorcycle;
+use App\Factory\MotorcycleFactory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class MotorcycleTest extends ApiTestCase
 {
+    use ResetDatabase, Factories;
+
+    public function testGetCollection(): void
+    {
+        static::$alwaysBootKernel = true;
+
+        $client = static::createClient();
+
+        MotorcycleFactory::createMany(100);
+
+        $response = $client->request('GET', '/api/motorcycles');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Motorcycle',
+            '@id' => '/api/motorcycles',
+            '@type' => 'Collection',
+            'totalItems' => 100,
+            'view' => [
+                '@id' => '/api/motorcycles?page=1',
+                '@type' => 'PartialCollectionView',
+                'first' => '/api/motorcycles?page=1',
+                'last' => '/api/motorcycles?page=4',
+                'next' => '/api/motorcycles?page=2',
+            ],
+        ]);
+
+        $this->assertCount(30, $response->toArray()['member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Motorcycle::class);
+    }
+
     public function testCreateMotorcycle(): void
     {
         static::$alwaysBootKernel = true;
